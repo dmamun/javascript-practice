dbe01146c3a38212763439cc5e9b7ff0e45b6180
dmamun <dmamun3311@gmail.com>
2023-08-02 16:24:48 +0600

    problems on loop and if-else

---
+++ if-else-problems.js
@@ -0,0 +1,83 @@
+// check if a given number is positive ,negative or zero.
+// determine if a person is eligible to vote based on their age.
+// check if a number is even or odd.
+// calculate the grade based on a students exam score.
+// find the largest of three numbers using nested if-else statement.
+// check if a year is a leap year or not.
+// determine the type of a triangle based on its side lengths.
+
+// var num=8;
+// if(num>0){
+//     console.log("positive number");
+// }
+// else if(num<0){
+//     console.log("negative number");
+// }
+// else{
+//     console.log("the number is zero");
+// }
+// var age=20;
+// if(age>18){
+//     console.log("he can vote");
+// }
+// else{
+//     console.log("he cannot vote");
+// }
+// var num=31;
+// if(num%2==0){
+//     console.log("the number is even")
+// }
+// else{
+//     console.log("the number is an odd number");
+// }
+// var num=65;
+// if(num>80){
+//     console.log("A+")
+// }
+// else if(num>70 && num<80){
+//     console.log("A grade");
+
+// }
+// else if(num>60 && num<70){
+//     console.log("A- grade");
+
+// }
+// else{
+//     console.log("this performance is bad");
+// }
+
+// var num1=128;
+// var num2=65;
+// var num3=70;
+// if(num1>num2){
+//    if(num1>num3){
+//     console.log("num1")
+//    }
+//    else{
+//     console.log("num3")
+//    }
+// }
+// else{
+//     if(num2>num3){
+//         console.log("num2");
+//     }
+//     else{
+//         console.log('num3');
+//     }
+// }
+// var leapYear=2025;
+// if((leapYear%4==0 && leapYear%100!==0) ||(leapYear%400==0)){
+//     console.log("the year is leap Year");
+// }
+// else{
+//     console.log("not leap year");
+// }
+// var side1=25;
+// var side2=30;
+// var side3=25;
+// if((side1==side2)|| (side2==side3) ||(side3==side1)){
+//     console.log("The triangle is somobahu");
+// }
+// else{
+//     console.log("somobahu noy");
+// }
---
+++ loops-problems.js
@@ -0,0 +1,21 @@
+// print numbers from 1 to 10 using a for loop.
+// print even numbers from 1 to 20 using for loop.
+// calculate the sum of numbers from 1 to 100 using while loop.
+// print every third number 30 to 60 using a for loop.
+
+// for(var i=0;i<=10;i++){
+//     console.log(i);
+// }
+// for(var i=2;i<=20;i=i+2){
+//     console.log(i)
+// }
+// var sum=0;
+// for(var i=1;i<=100;i++){
+//     sum=sum+i;
+
+// }
+// console.log(sum);
+// for(var i=30;i<=60;i=i+3){
+//     console.log(i);
+
+// }
